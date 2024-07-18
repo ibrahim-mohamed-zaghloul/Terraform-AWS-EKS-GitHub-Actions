16f25d04baf0e89ad5c4b6c2e124efb295d8a98a
Ibrahim <ebrahem.mohamedzaghloul@gmail.com>
2024-07-18 12:19:52 +0300

    vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -45,19 +45,27 @@ resource "aws_security_group" "security-group" {
   vpc_id      = aws_vpc.vpc.id
   description = "Allowing SSH Access"
 
-  ingress {
-    from_port   = 22 #SSH
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = [
+    for port in [22, 8080, 9000, 9090, 80] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      ipv6_cidr_blocks = ["::/0"]
+      self             = false
+      prefix_list_ids  = []
+      security_groups  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   tags = {
     Name = var.sg-name
   }
