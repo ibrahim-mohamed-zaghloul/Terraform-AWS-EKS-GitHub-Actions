dec7d553db4beba8894d2032572e0d17cd9e2929
Ibrahim <ebrahem.mohamedzaghloul@gmail.com>
2024-07-18 12:00:08 +0300

    vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -43,29 +43,21 @@ resource "aws_route_table_association" "rt-association" {
 
 resource "aws_security_group" "security-group" {
   vpc_id      = aws_vpc.vpc.id
-  description = "Allowing Jenkins, Sonarqube, SSH Access"
+  description = "Allowing SSH Access"
 
-  ingress = [
-    for port in [22, 8080, 9000, 9090, 80] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
-      protocol         = "tcp"
-      ipv6_cidr_blocks = ["::/0"]
-      self             = false
-      prefix_list_ids  = []
-      security_groups  = []
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
-
+  ingress {
+    from_port   = 22 #SSH
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   tags = {
     Name = var.sg-name
   }
